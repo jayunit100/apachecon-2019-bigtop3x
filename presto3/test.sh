5ccb290dd7d61ff05c59b208b485caf87a380e22
Cloud User <jay@apache.org>
2019-08-29 18:30:42 +0000

    added test and deployment yaml fixes

--- presto3/test.sh
+++ presto3/test.sh
@@ -1,4 +1,6 @@
+
+
+kubectl create ns minio ; kubectl -n minio create secret generic my-minio-secret --from-literal=accesskey=minio --from-literal=secretkey=minio123 && helm install --set existingSecret=my-minio-secret stable/minio --namespace=minio --name=minio
+
 echo "installing via helm: minio"
-kubectl create ns presto3
-helm install --name minio stable/minio --namespace=presto3
-kubectl create -f ./ -n presto3
+kubectl create -f ./ -n minio
