3dd3facacc34661eaa8dc624c2b9fcc281ae4f9f
Cloud User <jay@apache.org>
2019-08-29 18:35:50 +0000

    add cleanup to test.sh

--- presto3/test.sh
+++ presto3/test.sh
@@ -1,3 +1,9 @@
+echo "ok to cleanup?"
+read x
+
+kubectl delete ns minio
+helm delete minio --purge
+
 
 
 kubectl create ns minio ; kubectl -n minio create secret generic my-minio-secret --from-literal=accesskey=minio --from-literal=secretkey=minio123 && helm install --set existingSecret=my-minio-secret stable/minio --namespace=minio --name=minio
