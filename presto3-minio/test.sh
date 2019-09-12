a15f764b61f9cd1fdb91c5e958f1409db0f57cbc
jay vyas <jayunit100.github@gmail.com>
2019-09-12 14:16:56 -0700

    Update test.sh

--- presto3-minio/test.sh
+++ presto3-minio/test.sh
@@ -6,7 +6,14 @@ helm delete minio --purge
 
 
 
-kubectl create ns minio ; kubectl -n minio create secret generic my-minio-secret --from-literal=accesskey=minio --from-literal=secretkey=minio123 && helm install --set existingSecret=my-minio-secret stable/minio --namespace=minio --name=minio
+kubectl create ns minio ; 
+# Sets up minio passwords...
+kubectl -n minio create secret generic my-minio-secret --from-literal=accesskey=minio --from-literal=secretkey=minio123 
+
+# Install minio
+helm install --set existingSecret=my-minio-secret stable/minio --namespace=minio --name=minio
 
 echo "installing via helm: minio"
+
+# Install presto w/ minio configured...
 kubectl create -f ./ -n minio
